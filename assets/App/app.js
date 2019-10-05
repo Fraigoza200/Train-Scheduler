73e45724de6a3b2a55f673613aa8961fbd97384a
Francisco Raigoza <raigoza.frankie@gmail.com>
2019-10-05 09:54:43 -0700

    edited javascript file

--- assets/App/app.js
+++ assets/App/app.js
@@ -1,15 +1,44 @@
-//Timer
-const getNext = (original, rate) => {
-    
-    const rateInSeconds = rate * 60
-​
-    const now = moment().unix()
-​
-    let lapse = original
-​
-    while (lapse < now) {
-      lapse += rateInSeconds
+document.getElementById('btn').addEventListener('click', e => {
+    e.preventDefault()
+
+    let trainInfo = {
+        trainName: document.getElementById('trainInfo').value,
+        Destination: document.getElementById('destination').value,
+        trainTime: document.getElementById('trainTime').value,
+        frequencies: document.getElementById('minutes').value
     }
-​
-    return moment((lapse + rate), 'X').format('MMMM, Do YYYY hh:mm a')
-  }
+    // console.log(trainInfo)
+
+    db
+        .collection('trains')
+        .doc(trainInfo.trainName)
+        .set(trainInfo)
+
+        document.getElementById('trainInfo').value = ''
+        document.getElementById('destination').value = ''
+        document.getElementById('trainTime').value = ''
+        document.getElementById('minutes').value = ''
+})
+
+db
+.collection('trains')
+.onSnapshot(({docs}) => {
+    docs.forEach(train =>{
+        console.log(train)
+        // let {trainName, Destination, trainTime,frequencies} = doc.data
+
+            let trainElem = document.createElement('class')
+            trainElem.innerHTML = `<tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>${trainInfo}</td>
+              <td>${destination}</td>
+              <td>${trainTime}</td>
+              <td>${minutes}
+            </tr>
+          </tbody>`
+
+          document.querySelector('.table').append(trainElem)
+    })
+})
+
